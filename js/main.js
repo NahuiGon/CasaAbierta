175d620640f10f7b5bc0dd715227112f2470565f
Luis Gonzalez <luisitos1234@gmail.com>
2023-02-08 22:20:42 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 });
+
+var aplicacion = new function(){
+    this.empleados = document.getElementById("usuarios");
+
+    this.Leer= function(){
+        var datos = "";
+
+        
+    }
+}
